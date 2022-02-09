3b62609190268ac5a4d1b76a8e78c881759298e7
oscargarciaporras <osligarpo92@gmail.com>
2022-02-08 21:36:57 -0500

    variables y tipos de datos 8-02-2022

--- main.js
+++ main.js
@@ -1,17 +1,116 @@
-console.log("Mensaje");
-console.warn("Advertencia");
-console.error("Eroor");
-console.group("Agrupar Mensaje");
+/*Tipo de dato null y undefined*/
+let variableExistente = null;
+let variableNoExistente = undefined;
 
-    let datos = [
-        {Nombre: "Miguel", Edad: 23},
-        {Nombre: "Raul", Edad:33 },
-        {Nombre: "JuanCarlos", Edad: 36 },
-        {Nombre: "oscar", Edad:29 }
-    ];
-    console.table(datos);
+console.log(typeof(variableExistente));
+console.log(typeof(variableNoExistente));
 
-console.groupEnd();
 
-console.log("%c Personalizado", "color:blue; font-family:cursive; font-size: 20px;");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de dato Bolean*/
+// let soySuProfesor = true;
+// let soyUnEstudiante = false;
+// console.log(`Verdadero: ${soySuProfesor} Falso: ${soyUnEstudiante}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de datos Number*/
+// let edad = 29;
+// let altura = 1.72;
+
+// let moneda = Intl.NumberFormat("de-DE", {style: "currency", currency: "COP"}).format(1000);
+
+// console.log(`
+//     Edad : ${edad},
+//     Altura: ${altura},
+//     Moneda: ${moneda}
+// \n`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tipo de datos String*/
+// let primerNombre = "Oscar";
+// let segundoNombre = 'Libardo';
+// let apellidos = ` Garcia Porras`;
+
+// console.log(primerNombre, segundoNombre, apellidos);
+// console.log(primerNombre+' "'+"'"+toString(555).value+"\\n'"+'"');
+// console.log(segundoNombre.concat(apellidos));
+// console.log(`${primerNombre} "'${segundoNombre}'" `);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// `// let nombre = `Oscar Libardo  Garcia Porras `;
+
+/****Dato almacenado en el cache del navegador 'localStorage' ****/
+// localStorage.setItem("Dato", "Libro de cocina");
+// let recuperar = localStorage.getItem("Dato");
+// console.log(recuperar);
+
+
+/****** Constantes en JS ********/
+// const libros = 'Libro de cocina';
+// // libros = "Matematica";
+// console.log(libros);
+
+
+
+/*******Declaracion de tipo de datos heredado del objeto*****/
+// let numero = new Number(1000000);
+// let nombre = new String("Hola");
+// console.log(numero);
